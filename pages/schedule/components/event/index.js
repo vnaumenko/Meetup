6be5ddfe27179e9ec16a90d0c09520d46edb71ec
Vyacheslav Naumenko <i@v-naumenko.ru>
2021-05-31 21:10:05 +0600

    feat(event): добавил модалку

--- pages/schedule/components/event/index.js
+++ pages/schedule/components/event/index.js
@@ -1,18 +1,109 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
 function Event({ event }) {
-  return <div className={'timetable-event'} key={event.id}>
-    <p className={'type'}>{event.type}</p>
-    <p className={'title'}>{event.title}</p>
-    <p className={'name'}>{event.speaker.name}</p>
-    <div className={'info'}>
-      <div className={'photo'}>
-        <img src={event.speaker.photo} alt={event.speaker.name}/>
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [form, updateForm] = useState({
+    name: '',
+    email: '',
+  });
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
+    console.log(form);
+  };
+
+  return (
+    <>
+      <div className={'timetable-event'} key={event.id}>
+        <p className={'type'}>{event.type}</p>
+        <p className={'title'}>{event.title}</p>
+        <p className={'name'}>{event.speaker.name}</p>
+        <div className={'info'}>
+          <div className={'photo'}>
+            <img src={event.speaker.photo} alt={event.speaker.name} />
+          </div>
+          <div className="desc">
+            <p>{event.description}</p>
+            <button className={'btn btn-primary'} onClick={openModal}>
+              Записаться
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="desc">
-        <p>{event.description}</p>
-        <button className={'btn btn-primary'}>Записаться</button>
-      </div>
-    </div>
-  </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className="modal"
+        overlayClassName="modal-overlay"
+        closeTimeoutMS={300}
+      >
+        <div className="title">
+          Вы записываетесь на
+          <h1>{event.title}</h1>
+          <time>27 июня, среда в 16:30</time>
+        </div>
+        <div className={'form'}>
+          <form onSubmit={submitForm}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Имя
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={form.name}
+                onChange={({ target }) => {
+                  updateForm((prevState) => {
+                    return {
+                      ...prevState,
+                      name: target.value,
+                    };
+                  });
+                }}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="form-label">
+                Рабочая почта
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={form.email}
+                onChange={({ target }) => {
+                  updateForm((prevState) => {
+                    return {
+                      ...prevState,
+                      email: target.value,
+                    };
+                  });
+                }}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary me-3">
+              Записаться
+            </button>
+            <button type="submit" className="btn btn-link" onClick={closeModal}>
+              Отмена
+            </button>
+          </form>
+        </div>
+      </Modal>
+    </>
+  );
 }
 
 export default Event;
--- pages/schedule/index.js
+++ pages/schedule/index.js
@@ -1,6 +1,7 @@
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 import ScheduleIcon from '../../public/schedule.svg';
-import Event from "./components/event";
+import Event from './components/event';
 
 function Schedule() {
   const events = [
@@ -8,66 +9,71 @@ function Schedule() {
       id: 1,
       type: 'lifestyle',
       title: 'Как варить пельмени?',
-      description: 'Товарищи! консультация с широким активом влечет за собой процесс внедрения и модернизации существенных финансовых и административных условий.',
+      description:
+        'Товарищи! консультация с широким активом влечет за собой процесс внедрения и модернизации существенных финансовых и административных условий.',
       speaker: {
         photo: 'https://placekitten.com/100/100',
         name: 'Дмитрий Дмитриев',
-      }
+      },
     },
     {
       id: 2,
       type: 'lifestyle',
       title: 'Как варить манты?',
-      description: 'Товарищи! консультация с широким активом влечет за собой процесс внедрения и модернизации существенных финансовых и административных условий.',
+      description:
+        'Товарищи! консультация с широким активом влечет за собой процесс внедрения и модернизации существенных финансовых и административных условий.',
       speaker: {
         photo: 'https://placekitten.com/100/100',
         name: 'Дмитрий Дмитриев',
-      }
+      },
     },
   ];
 
   const renderTimetable = () => {
+    const renderEvents = () =>
+      events.map((event) => {
+        return (
+          <div className="col-12 col-lg-6">
+            <Event event={event} />
+          </div>
+        );
+      });
 
-    const renderEvents = () => events.map((event) => {
-      return <div className="col-12 col-lg-6">
-        <Event event={event} />
-      </div>
-    });
-
-    return <div className="timetable">
-      <div className="timetable-day">
-        <p className="timetable-day_title">6 июня, среда</p>
-        <div className={'row'}>
-          {renderEvents()}
+    return (
+      <div className="timetable">
+        <div className="timetable-day">
+          <p className="timetable-day_title">6 июня, среда</p>
+          <div className={'row'}>{renderEvents()}</div>
         </div>
       </div>
-    </div>
-  }
+    );
+  };
 
   return (
     <div id={'schedule'} className={'schedule'}>
       <div className="container">
         <ul className={'minimenu'}>
           <li>
-            <Link href={"/"}>Вернуться на главную</Link>
+            <Link href={'/'}>Вернуться на главную</Link>
           </li>
           <li>
-            <Link href={"/faq"}>FAQ</Link>
+            <Link href={'/faq'}>FAQ</Link>
           </li>
         </ul>
-        <h1 className={'title'}>
-          Расписание
-        </h1>
-        <p className={'subtitle-text'}>Товарищи! консультация с широким активом влечет за собой процесс внедрения и
-          модернизации существенных финансовых и административных условий. Не следует, однако забывать, что дальнейшее
-          развитие различных форм деятельности требуют от нас анализа позиций, занимаемых участниками в отношении
-          поставленных задач. Не следует, однако забывать, что сложившаяся структура организации обеспечивает широкому
-          кругу (специалистов) участие в формировании дальнейших направлений развития.</p>
+        <h1 className={'title'}>Расписание</h1>
+        <p className={'subtitle-text'}>
+          Товарищи! консультация с широким активом влечет за собой процесс внедрения и модернизации
+          существенных финансовых и административных условий. Не следует, однако забывать, что
+          дальнейшее развитие различных форм деятельности требуют от нас анализа позиций, занимаемых
+          участниками в отношении поставленных задач. Не следует, однако забывать, что сложившаяся
+          структура организации обеспечивает широкому кругу (специалистов) участие в формировании
+          дальнейших направлений развития.
+        </p>
         {renderTimetable()}
-        <ScheduleIcon className={'illustration'}/>
+        <ScheduleIcon className={'illustration'} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Schedule
+export default Schedule;
